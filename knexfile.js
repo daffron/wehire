a6ad6b6aca673a3df01b36ded7cad13e12ae941f
daffron <daffron92@gmail.com>
2017-06-27 19:03:41 +1200

    database config file update

--- knexfile.js
+++ knexfile.js
@@ -26,15 +26,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     }
