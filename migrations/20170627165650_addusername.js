a80440ee6d16bed0ffae646bab22a20c4a887f11
daffron <daffron92@gmail.com>
2017-07-02 14:55:26 +1200

    minor linting

--- migrations/20170627165650_addusername.js
+++ migrations/20170627165650_addusername.js
@@ -1,11 +1,11 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return knex.schema.table('users', t => {
     t.string('user_name')
   })
 }
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.table('users', t => {
     t.dropColumn('user_name')
   })
