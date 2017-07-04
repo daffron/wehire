648d5cc7a7d1146bc620df8070cd2fb1c2d7f96c
keewhy <samhoulahan625@gmail.com>
2017-07-04 15:23:49 +1200

    locations working

--- client/components/CreateListing.jsx
+++ client/components/CreateListing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import locations from './locations.json'
 class CreateListing extends React.Component {
   constructor (props) {
     super(props)
@@ -7,9 +7,9 @@ class CreateListing extends React.Component {
       title: '',
       description: '',
       price: 0,
-      perHour: false,                                                                                                                                                                                                                                                                                                                                                                                                                                  
+      perHour: true,
       perDay: false,
-      unavailableDates: [],
+      unavailableDates: ['15/7/17'],
       images: '',
       despositAmount: 0,
       location: '',
@@ -17,18 +17,38 @@ class CreateListing extends React.Component {
       whatsIncludes: []
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
   }
+
   handleChange (evt) {
-    this.setState({
-      [evt.target.name]: evt.target.value
-    })
+    this.setState({[evt.target.name]: evt.target.value})
   }
+
+  handleSelect (evt) {
+    this.setState({perHour: false, perDay: true})
+  }
+
   render () {
     console.log(this.state)
     return (
       <div>
         <h2>Create Listing</h2>
-        Title:<input name='title' onChange={this.handleChange} />
+        Title:<input name='title' onChange={this.handleChange} /><br />
+        Description:<input name='description' onChange={this.handleChange} /><br />
+        Price:<input name='price' onChange={this.handleChange} />
+        <select onChange={this.handleSelect}>
+          <option value='false'>Per Hour </option>
+          <option value='true'> Per Day </option>
+        </select>
+        Unavailable Dates:<input name='unavailableDates' onChange={this.handleChange} value={['15/7/17']} /><br />
+        Images:<input name='images' onChange={this.handleChange} /><br />
+        Location:<select name='location' onChange={this.handleChange}>
+          {locations.locations.map((name, i) => {
+            return (
+              <option value={name.regions} key={i}>{name.regions}</option>
+            )
+          })}
+        </select>
       </div>
     )
   }
