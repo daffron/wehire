f3e034cec35ddbee22c03f291f29ca36f3cccb35
keewhy <samhoulahan625@gmail.com>
2017-07-10 11:38:39 +1200

    removed blank line

--- client/actions/locations.js
+++ client/actions/locations.js
@@ -2,7 +2,7 @@ import {RECEIVE_LOCATIONS} from './variables'
 import request from '../utils/tokenApi'
 import {waiting, notWaiting} from './index'
 
-const receiveLocations = (locations) => {
+const receiveLocations = locations => {
   return {
     type: RECEIVE_LOCATIONS,
     locations
