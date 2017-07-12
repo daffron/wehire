8a6a3820bc7bb49074a4918d2af490337dc0e8f3
daffron <daffron92@gmail.com>
2017-07-12 18:32:46 +1200

    added cycle images function

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -7,20 +7,35 @@ class Listing extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      listingId: props.match.params.id
+      listingId: props.match.params.id,
+      image: 0
     }
+    this.cycleImages = this.cycleImages.bind(this)
   }
 
   componentDidMount () {
     this.props.getListing(this.state.listingId)
   }
 
+  cycleImages () {
+    switch (this.state.image) {
+      case 0:
+        this.setState({image: 1})
+        break
+      case 1:
+        this.setState({image: 2})
+        break
+      case 2:
+        this.setState({image: 0})
+    }
+  }
   render () {
     return (
       <div className='listing'>
         <div className='row'>
           <div className='col-sm-8'>
-            {this.props.listing.images && <img src={this.props.listing.images[0]} />}
+            {this.props.listing.images && <img src={this.props.listing.images[this.state.image]} />}
+            <button onClick={this.cycleImages}>Next</button>
           </div>
         </div>
       </div>
