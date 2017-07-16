a6f8e9c22b2bea8d46f9a516200ddd18649979ae
keewhy <samhoulahan625@gmail.com>
2017-07-16 21:14:46 +1200

    added frontend route

---
+++ client/actions/booking.js
@@ -0,0 +1,16 @@
+import {NEW_BOOKING} from './variables'
+import request from '../utils/tokenApi'
+import {waiting, notWaiting} from './index'
+import {error} from './error'
+
+export function addBooking (listingId, userId, unavailableDates, bookedDates) {
+  return dispatch => {
+    dispatch(waiting())
+    request('put', `/listing/${listingId}/booking`, {unavailable_dates: unavailableDates, booking: {booked_user: userId, booked_dates: bookedDates}})
+    .then(result => {
+      if (result.error) return dispatch(error(result.error))
+      dispatch(notWaiting())
+    })
+  }
+}
+
