55d30fa4e8cbb04502524674ae18b85573951460
keewhy <samhoulahan625@gmail.com>
2017-07-18 19:43:10 +1200

    added components

---
+++ client/components/Booking.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Booking extends React.Component {
+    constructor (props) {
+    super (props)
+  }
+}
+
+export default Booking
---
+++ client/components/Bookings.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Bookings extends React.Component {
+    constructor (props) {
+    super (props)
+
+    }
+}
+
+export default Bookings
