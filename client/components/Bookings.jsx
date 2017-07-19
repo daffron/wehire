dab55df7ce48d2dd9deb7107157560be1933fa41
keewhy <samhoulahan625@gmail.com>
2017-07-20 10:30:28 +1200

    fixed naming

--- client/components/Bookings.jsx
+++ client/components/Bookings.jsx
@@ -12,51 +12,51 @@ class Bookings extends React.Component {
 
   render () {
     return (
-    <div className='users-bookings'>
+      <div className='users-bookings'>
         <h3 className="text-center">My Bookings</h3>
         <h3>Renting from</h3>
-      <table className='booking-table'>
-        <tr>
-          <th>Listing ID</th>
-          <th>Listing Title</th>
-          <th>Date From</th>
-          <th>Date to</th>
-        </tr>
-  {this.props.rentingfrom.map(booking => {
-    return (
-      <tbody>
+        <table className='booking-table'>
           <tr>
-            <td>{booking.listing_id}</td>
-            <td>{booking.listing_name}</td>
-            <td>{booking.booked_dates[0]} </td>
-            <td>{moment(booking.booked_dates[booking.booked_dates.length - 1]).format('DD/MM/YYYY')}</td>
+            <th>Listing ID</th>
+            <th>Listing Title</th>
+            <th>Date From</th>
+            <th>Date to</th>
           </tr>
-      </tbody>
-    )
-  })}
-    </table>
-      <h3>Renting To</h3>
-      <table className='booking-table'>
-        <tr>
-          <th>Listing ID</th>
-          <th>Listing Title</th>
-          <th>Date From</th>
-          <th>Date To</th>
-        </tr>
-    {this.props.rentingto.map(booking => {
-      return (
-        <tbody>
-            <tr>
-              <td>{booking.listing_id}</td>
-              <td>{booking.listing_name}</td>
-              <td>{booking.booked_dates[0]} </td>
-              <td>{moment(booking.booked_dates[booking.booked_dates.length - 1]).format('DD/MM/YYYY')}</td>
-            </tr>
-        </tbody>
-      )
-    })}
-      </table>
-    </div>
+      {this.props.rentingfrom.map(booking => {
+        return (
+          <tbody>
+              <tr>
+                <td>{booking.listing_id}</td>
+                <td>{booking.listing_name}</td>
+                <td>{booking.booked_dates[0]} </td>
+                <td>{moment(booking.booked_dates[booking.booked_dates.length - 1]).format('DD/MM/YYYY')}</td>
+              </tr>
+          </tbody>
+        )
+      })}
+        </table>
+        <h3>Renting To</h3>
+        <table className='booking-table'>
+          <tr>
+            <th>Listing ID</th>
+            <th>Listing Title</th>
+            <th>Date From</th>
+            <th>Date To</th>
+          </tr>
+      {this.props.rentingto.map(booking => {
+        return (
+          <tbody>
+              <tr>
+                <td>{booking.listing_id}</td>
+                <td>{booking.listing_name}</td>
+                <td>{booking.booked_dates[0]} </td>
+                <td>{moment(booking.booked_dates[booking.booked_dates.length - 1]).format('DD/MM/YYYY')}</td>
+              </tr>
+          </tbody>
+        )
+      })}
+        </table>
+      </div>
     )
   }
 }
